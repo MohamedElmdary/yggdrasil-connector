bd655e383eb19fc864d464b27dcbb96396761a52
MohamedElmdary <engm5081@gmail.com>
2021-09-07 11:29:36 +0200

    Added loadUI

--- main.go
+++ main.go
@@ -1,19 +1,59 @@
 package main
 
 import (
-	"fyne.io/fyne/app"
-	"fyne.io/fyne/widget"
-	// "github.com/MohamedElmdary/yggdrasil-connector/src/yggdrasil"
+	"fmt"
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/layout"
+	"fyne.io/fyne/v2/widget"
+	"github.com/MohamedElmdary/yggdrasil-connector/src/yggdrasil"
 )
 
+func loadUI(peers map[string][]string) fyne.CanvasObject {
+	items := []fyne.CanvasObject{}
+
+	for country, values := range peers {
+		if len(values) == 0 {
+			continue
+		}
+
+		countryLabel := canvas.NewText(country, color.White)
+		countryLabel.Alignment = fyne.TextAlignCenter
+		countryLabel.TextStyle = fyne.TextStyle{Bold: true}
+		countryLabel.TextSize = 20
+
+		items = append(items, countryLabel)
+
+		for _, value := range values {
+			checkbox := widget.NewCheck(value, func(checked bool) {
+				fmt.Println(checked)
+			})
+			items = append(items, checkbox)
+		}
+	}
+
+	return container.NewVScroll(
+		container.New(
+			layout.NewHBoxLayout(),
+			container.New(
+				layout.NewVBoxLayout(),
+				items...,
+			),
+		),
+	)
+}
+
 func main() {
-	// peers := yggdrasil.GetPeers()
-	// fmt.Println(peers)
+	peers := yggdrasil.GetPeers()
 
 	application := app.New()
 	window := application.NewWindow("Hello world")
+	window.Resize(fyne.NewSize(250, 600))
 
-	window.SetContent(widget.NewLabel("Hello"))
+	window.SetContent(loadUI(peers))
 	window.ShowAndRun()
-
 }
